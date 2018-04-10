b72db6e8d9ae7fbfe5308e1259bbc7182fb3e0ee
ricoarisandyw <arisandyrico@gmail.com>
2018-04-10 10:58:48 +0700

    Activate Library

---
+++ libraryku/src/main/java/com/javacreativeapps/libraryku/Player.java
@@ -0,0 +1,13 @@
+package com.javacreativeapps.libraryku;
+
+/**
+ * Created by wijaya on 4/10/2018.
+ */
+
+public class Player {
+    String name;
+    String role;
+    String skill;
+    int hp;
+    int mp;
+}
